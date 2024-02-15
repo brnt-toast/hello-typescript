e8187dd50b3bb92aabaef1f7006affd079d6c3ad
brnt-toast <yoyo.beerme@gmail.com>
2024-02-14 20:44:48 -0500

    feathersjs.com: Our First App

--- src/hello-world/feathers/app.ts
+++ src/hello-world/feathers/app.ts
@@ -28,3 +28,38 @@ class MessageService {
         return message
     }
 }
+
+// service registering
+type ServiceTypes = {
+    messages: MessageService
+}
+
+const app = feathers<ServiceTypes>()
+
+// register message service 
+app.use('messages', new MessageService())
+
+// log every time a new message has been created
+app.service('messages').on('created', (message: Message) => {
+    console.log('A new message has been created', message)
+})
+
+// create messages then log all existing messages
+const main = async () => {
+    // create new message 
+    await app.service('messages').create({
+        text: "Hello Feathers"
+    })
+
+    //another message
+    await app.service('messages').create({
+        text: 'Hello again'
+    })
+
+    // find all messages
+    const messages = await app.service('messages').find()
+
+    console.log('All messages', messages)
+}
+
+main()
